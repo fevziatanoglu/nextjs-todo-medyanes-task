87febd48f7995df9bbffc0762c9f28435ce48f05
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-07-07 19:22:59 +0300

    refactor PUT and DELETE API routes for todos; update params handling to use Promise for improved async behavior

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -3,16 +3,17 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { authOptions } from "@/lib/authOptions";
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
     const session = await getServerSession(authOptions)
+    const { id } = await params;
     if (!session?.user?.id) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
     const { title, completed } = await req.json();
     const todo = await prisma.todo.update({
-      where: { id: params.id, userId: session.user.id },
+      where: { id, userId: session.user.id },
       data: { title, completed },
     });
     return NextResponse.json({ todo });
@@ -21,15 +22,16 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }>  }) {
   try {
     const session = await getServerSession(authOptions)
     if (!session?.user?.id) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
+    const { id } = await params;
     await prisma.todo.delete({
-      where: { id: params.id, userId: session.user.id },
+      where: { id, userId: session.user.id },
     });
     return NextResponse.json({ success: true });
   } catch (error) {
