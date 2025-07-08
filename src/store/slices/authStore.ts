8d667f9fe9e14593124546e61434dadcddcd79b9
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-07-08 10:55:20 +0300

    add toast notifications for signin, signup, and logout actions in authStore

--- src/store/slices/authStore.ts
+++ src/store/slices/authStore.ts
@@ -2,6 +2,7 @@ import { User } from '@/types/user';
 import { SigninSchema } from '@/validation/signInValidation';
 import { SignupSchema } from '@/validation/signUpValidation';
 import { signIn, signOut } from 'next-auth/react';
+import { toast } from 'react-toastify';
 
 export type AuthStore = {
   user: User | null;
@@ -27,11 +28,14 @@ export const createAuthSlice = (set: any, get: any): AuthStore => ({
       });
       if (res?.error) {
         set({ error: res.error, user: null });
+        toast.error(res.error || "Signin failed");
       } else {
         set({ error: null });
+        toast.success('Signin successful');
       }
     } catch (err) {
       set({ error: 'Something went wrong, please try again later', user: null });
+      toast.error('Signin failed, please try again later');
     } finally {
       set({ isUserLoading: false });
       get().closeModal()
@@ -51,12 +55,15 @@ export const createAuthSlice = (set: any, get: any): AuthStore => ({
       const data = await res.json();
       if (!res.ok) {
         set({ error: data.error || 'Signup failed', user: null });
+        toast.error(data.error || 'Signup failed');
       }
       if (data.user) {
         set({ error: null });
+        toast.success('Signup successful');
       }
     } catch (err) {
       set({ error: 'Something went wrong, please try again later', user: null });
+      toast.error('Signup failed, please try again later');
     } finally {
       set({ isUserLoading: false });
       get().closeModal()
@@ -69,6 +76,7 @@ export const createAuthSlice = (set: any, get: any): AuthStore => ({
       set({ user: null, error: null });
     } catch (err) {
       set({ error: 'Something went wrong, please try again later', user: null });
+      toast.error('Logout failed, please try again later');
     } finally {
       set({ isUserLoading: false });
     }
