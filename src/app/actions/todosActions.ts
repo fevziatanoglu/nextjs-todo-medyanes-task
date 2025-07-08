dd7624b145a02f826faaeb7367674cd317bde58c
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-07-08 12:37:59 +0300

    refactor todo actions to use direct title parameter; return created, updated, and deleted todos

--- src/app/actions/todosActions.ts
+++ src/app/actions/todosActions.ts
@@ -3,7 +3,6 @@
 import { prisma } from "@/lib/prisma";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
-import { revalidatePath } from "next/cache";
 
 export async function fetchTodosAction() {
   const session = await getServerSession(authOptions);
@@ -14,39 +13,38 @@ export async function fetchTodosAction() {
   return todos;
 }
 
-export async function addTodoAction(formData: FormData) {
+export async function addTodoAction(title:string) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id) throw new Error("Unauthorized");
 
-  const title = formData.get("title") as string;
   if (!title) throw new Error("Title is required");
 
-  await prisma.todo.create({
+  const todo = await prisma.todo.create({
     data: { title, completed: false, userId: session.user.id },
   });
 
-  revalidatePath("/home-with-actions");
+  return todo;
 }
 
 export async function updateTodoAction(id: string, updates: { title?: string; completed?: boolean }) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id) throw new Error("Unauthorized");
 
-  await prisma.todo.update({
+  const updatedTodo = await prisma.todo.update({
     where: { id, userId: session.user.id },
     data: updates,
   });
 
-  revalidatePath("/home-with-actions");
+  return updatedTodo
 }
 
 export async function deleteTodoAction(id: string) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id) throw new Error("Unauthorized");
 
-  await prisma.todo.delete({
+  const deletedTodo =  await prisma.todo.delete({
     where: { id, userId: session.user.id },
   });
 
-  revalidatePath("/home-with-actions");
+  return deletedTodo
 }
